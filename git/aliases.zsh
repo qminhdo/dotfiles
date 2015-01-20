320a993e390d43a2440351eeb354129ca19d28e0
Minh Do <qminhdo@users.noreply.github.com>
2015-01-20 17:59:38 -0600

    add aliases for git

---
+++ git/aliases.zsh
@@ -0,0 +1,20 @@
+alias g="git "
+alias gs='git status '
+alias ga='git add '
+alias gaa='git add --all'
+alias gc='git commit'
+alias gb='git branch '
+alias gp='git push '
+alias gd='git diff'
+alias go='git checkout '
+alias gco='git checkout '
+alias gm='git merge '
+alias gk='gitk --all'
+alias gx='gitx --all'
+alias gt='git tag'
+alias gr='git remote'
+alias got='git '
+alias get='git '
+alias gci="git pull --rebase && rake && git push"
+alias gh="git log --graph --pretty=format:'%C(yellow)%h%Creset -%C(bold yellow)%d%Creset %C(white)%s%Creset %Cgreen(%cr) %C(dim white)<%an>%Creset' --abbrev-commit --date=relative"
+alias gh1="git log --graph --abbrev-commit --decorate --format=format:'%C(bold yellow)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
